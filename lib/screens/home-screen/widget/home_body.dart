9f37424b84e162d64d18286f1319c675fd111168
kingKamran <kamranghazi850@gmail.com>
2021-06-08 10:52:54 +0430

    best box home Commit

--- lib/screens/home-screen/widget/home_body.dart
+++ lib/screens/home-screen/widget/home_body.dart
@@ -10,7 +10,7 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
+      child: SingleChildScrollView(
         child: Column(
           children: [
             Logo(),
--- lib/screens/home-screen/widget/resturants_homeList.dart
+++ lib/screens/home-screen/widget/resturants_homeList.dart
@@ -11,21 +11,42 @@ class _ResturantsHomeListState extends State<ResturantsHomeList> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 220,
-      color: Colors.amber,
+      height: MediaQuery.of(context).size.width * 0.6,
+      padding: EdgeInsets.all(5),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('بهترین رستوران ها'),
-              Text('بازم هست'),
-            ],
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'بهترین رستوران ها',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    'بازم هست',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Expanded(
+            flex: 4,
             child: ListView(
               scrollDirection: Axis.horizontal,
               physics: BouncingScrollPhysics(),
@@ -38,7 +59,25 @@ class _ResturantsHomeListState extends State<ResturantsHomeList> {
                   address: 'خیابان آزادی - روبرو بانک ملت',
                   peykPrice: 1500,
                   img: 'assets/images/food2.jpg',
-                )
+                ),
+                ResturantBox(
+                  id: 0,
+                  name: 'رستوان ریحون',
+                  desc:
+                      'جوجه کباب بدون استخوان، فیله ماهی، میگو کبابی، کباب ترش، کباب چنجه ،کوبیده، قرمه سبزی، کباب برگ',
+                  address: 'خیابان آزادی - روبرو بانک ملت',
+                  peykPrice: 1500,
+                  img: 'assets/images/food2.jpg',
+                ),
+                ResturantBox(
+                  id: 0,
+                  name: 'رستوان ریحون',
+                  desc:
+                      'جوجه کباب بدون استخوان، فیله ماهی، میگو کبابی، کباب ترش، کباب چنجه ،کوبیده، قرمه سبزی، کباب برگ',
+                  address: 'خیابان آزادی - روبرو بانک ملت',
+                  peykPrice: 1500,
+                  img: 'assets/images/food2.jpg',
+                ),
               ],
             ),
           ),
@@ -68,62 +107,112 @@ class ResturantBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            flex: 4,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Image.asset(
-                    img,
-                    height: 150,
-                    width: 150,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: MediaQuery.of(context).size.width * 0.6,
+        padding: EdgeInsets.all(5),
+        margin: EdgeInsets.all(5),
+        width: MediaQuery.of(context).size.width * 0.92,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        image: DecorationImage(
+                          image: AssetImage(img),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: Text(
+                              desc,
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(name),
+                      Icon(
+                        Icons.sports_motorsports_outlined,
+                        color: Color(0xFFe91e63),
+                      ),
+                      SizedBox(width: 5),
                       Text(
-                        desc,
-                        maxLines: 3,
+                        '${peykPrice.toInt()}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
                       ),
                     ],
                   ),
-                )
-              ],
+                  Text(
+                    address,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.greenAccent[700],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.sports_motorsports_outlined),
-                    Text('${peykPrice}'),
-                  ],
-                ),
-                Text(address),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
