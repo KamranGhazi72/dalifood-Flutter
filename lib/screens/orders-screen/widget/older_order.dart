9501495a3228056a1fa5006f97b11d268c683b88
kingKamran <kamranghazi850@gmail.com>
2021-06-09 16:20:32 +0430

    Orders Page Commit

---
+++ lib/screens/orders-screen/widget/older_order.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+
+class OlderOrder extends StatelessWidget {
+  bool status;
+  OlderOrder({
+    Key? key,
+    required this.status,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.only(
+        right: 10,
+        top: 10,
+        bottom: 10,
+        left: 10,
+      ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFFEEEEEE),
+            blurRadius: 2,
+            spreadRadius: 3,
+          )
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      // height: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width * 0.92,
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 0.5,
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Container(
+                    height: MediaQuery.of(context).size.width * 0.25,
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/fsicon.png'),
+                          fit: BoxFit.contain),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'کترینگ ترنج',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.date_range_outlined,
+                                size: 17,
+                                color: Colors.black54,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '1399/3/2',
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '|',
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.watch_later_outlined,
+                                size: 17,
+                                color: Colors.black54,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '13:23',
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.where_to_vote_outlined,
+                            size: 19,
+                          ),
+                          SizedBox(width: 5),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: Text(
+                              'مهران - پیامبر - بلوار کاشانی - روبروی پارک یاران',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'خوراک جوجه کباب ممتاز',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '26000',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'تومن',
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 5, bottom: 20, left: 10),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Color(0xFFEEEEEE),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'مجموع',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '28000',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'تومن',
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 40),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.width * 0.12,
+              child: status
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'امتیاز شما به این سفارش:',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              '4',
+                              style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            const SizedBox(width: 1),
+                            Icon(
+                              Icons.star_rate_outlined,
+                              color: Color(0xFFFFD700),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  : ElevatedButton(
+                      onPressed: () {},
+                      child:
+                          Text('شما هنوز درباره این سفارش خود نظر نداده اید'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFe91e63),
+                        // padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        textStyle: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/orders-screen/widget/order_body.dart
+++ lib/screens/orders-screen/widget/order_body.dart
@@ -1,3 +1,4 @@
+import 'package:dali_food/screens/orders-screen/widget/older_order.dart';
 import 'package:flutter/material.dart';
 
 class OrderBody extends StatelessWidget {
@@ -5,6 +6,34 @@ class OrderBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.95,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('سفارش فعال'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green[600],
+                    // padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    textStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              OlderOrder(status: false),
+              OlderOrder(status: true),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
